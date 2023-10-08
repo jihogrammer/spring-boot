6b32db12b46aece7c3e7de8701241519e0a66c96
jihogrammer <jihogrammer@gmail.com>
2023-10-09 01:18:01 +0900

    add response html servlet

---
+++ src/main/java/dev/jihogrammer/servlet/basic/response/ResponseHTMLServlet.java
@@ -0,0 +1,28 @@
+package dev.jihogrammer.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/response-html")
+public class ResponseHTMLServlet extends HttpServlet {
+    @Override
+    protected void service(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        String html = """
+                <html>
+                <body>
+                  <h1>HTML</h1>
+                </body>
+                </html>
+                """;
+        response.getWriter().write(html);
+    }
+}
