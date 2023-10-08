c9f530819b09818197c954c9c57c0501af891755
jihogrammer <jihogrammer@gmail.com>
2023-10-08 22:36:10 +0900

    add request parameter servlet

---
+++ src/main/java/dev/jihogrammer/servlet/basic/request/RequestParameterServlet.java
@@ -0,0 +1,37 @@
+package dev.jihogrammer.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@WebServlet(name = "requestParameterServlet", urlPatterns = "/request-parameter")
+@Slf4j
+public class RequestParameterServlet extends HttpServlet {
+    @Override
+    protected void service(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+        printSingleValueParameters(request);
+        printMultiValueParameters(request);
+    }
+
+    private void printSingleValueParameters(final HttpServletRequest request) {
+        log.info("--- SINGLE VALUE PARAMETERS ---");
+        request.getParameterNames().asIterator()
+                .forEachRemaining(name -> log.info("param {}: {}", name, request.getParameter(name)));
+    }
+
+    private void printMultiValueParameters(final HttpServletRequest request) {
+        log.info("--- MULTI VALUES PARAMETERS ---");
+        request.getParameterNames().asIterator()
+                .forEachRemaining(name -> {
+                    String[] values = request.getParameterValues(name);
+                    if (values.length > 1) {
+                        log.info("{}: {}", name, values);
+                    }
+                });
+    }
+}
