96d93aa7965a94c5c0d63d864df0cb438436bda2
jihogrammer <jihogrammer@gmail.com>
2023-10-15 21:16:52 +0900

    improve controller

--- spring-mvc-application/src/main/java/dev/jihogrammer/spring_mvc/controller/MemberController.java
+++ spring-mvc-application/src/main/java/dev/jihogrammer/spring_mvc/controller/MemberController.java
@@ -2,11 +2,12 @@ package dev.jihogrammer.spring_mvc.controller;
 
 import dev.jihogrammer.member.Members;
 import dev.jihogrammer.member.model.Member;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/members")
@@ -17,29 +18,27 @@ public class MemberController {
         this.members = members;
     }
 
-    @RequestMapping("/new-form")
-    public ModelAndView newForm() {
-        return new ModelAndView("new-form");
+    @GetMapping("/new-form")
+    public String newForm() {
+        return "new-form";
     }
 
-    @RequestMapping("/save")
-    public ModelAndView save(final HttpServletRequest request, final HttpServletResponse response) {
-        String username = request.getParameter("username");
-        int age = Integer.parseInt(request.getParameter("age"));
+    @PostMapping("/save")
+    public String save(
+            @RequestParam("username") final String username,
+            @RequestParam("age") final int age,
+            final Model model
+    ) {
         Member member = new Member(username, age);
-
         Member newMember = members.save(member);
+        model.addAttribute("newMember", newMember);
 
-        ModelAndView modelAndView = new ModelAndView("save-result");
-        modelAndView.addObject("newMember", newMember);
-
-        return modelAndView;
+        return "save-result";
     }
 
-    @RequestMapping
-    public ModelAndView members() {
-        ModelAndView modelAndView = new ModelAndView("list");
-        modelAndView.addObject("members", members.findAll());
-        return modelAndView;
+    @GetMapping
+    public String members(final Model model) {
+        model.addAttribute("members", members.findAll());
+        return "list";
     }
 }
