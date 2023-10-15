c190d9e266957e62949fa7e45f5af1a6f6194977
jihogrammer <jihogrammer@gmail.com>
2023-10-15 19:15:39 +0900

    create spring-mvc-application module

---
+++ spring-mvc-application/src/main/java/dev/jihogrammer/spring_mvc/SpringMVCApplication.java
@@ -0,0 +1,11 @@
+package dev.jihogrammer.spring_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMVCApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(SpringMVCApplication.class, args);
+    }
+}
