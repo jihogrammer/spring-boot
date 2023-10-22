97acf46514bea22c5a6b76e6a436d66cc39cb9ea
jihogrammer <jihogrammer@gmail.com>
2023-10-22 14:54:41 +0900

    create item-service application module

---
+++ item-service/src/main/java/dev/jihogrammer/items/ItemsApplication.java
@@ -0,0 +1,11 @@
+package dev.jihogrammer.items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(ItemsApplication.class, args);
+    }
+}
