004eb25b515313869b12b4373b757f26187b7ffb
jihogrammer <jihogrammer@gmail.com>
2023-11-23 11:11:30 +0900

    create thymeleaf module

---
+++ thymeleaf/src/main/java/dev/jihogrammer/thymeleaf/ThymeleafApplication.java
@@ -0,0 +1,11 @@
+package dev.jihogrammer.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(ThymeleafApplication.class, args);
+    }
+}
