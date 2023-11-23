977076ab687a2142fb18671e528d4bb5139b3c92
jihogrammer <jihogrammer@gmail.com>
2023-11-23 14:14:14 +0900

    feat basic variables

--- thymeleaf/src/main/java/dev/jihogrammer/thymeleaf/basic/BasicController.java
+++ thymeleaf/src/main/java/dev/jihogrammer/thymeleaf/basic/BasicController.java
@@ -1,16 +1,20 @@
 package dev.jihogrammer.thymeleaf.basic;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/basic")
@@ -40,5 +44,28 @@ public class BasicController {
         model.addAttribute("userMap", Map.of(userA.getName(), userA, userB.getName(), userB));
 
         return "/basic/variable";
+    }
+
+    @Component("helloComponent")
+    public static class HelloComponent {
+        public String hello(final Object data) {
+            return "Hello " + data;
+        }
+    }
+
+    @GetMapping("/objects")
+    public String objects(
+        final HttpSession session,
+        final Model model,
+        final HttpServletRequest request,
+        final HttpServletResponse response,
+        final Locale locale
+    ) {
+        session.setAttribute("sessionData", "Hello Session!");
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+        model.addAttribute("locale", locale);
+        return "/basic/objects";
     }
 }
