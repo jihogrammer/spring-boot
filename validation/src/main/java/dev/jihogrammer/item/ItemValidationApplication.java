6ca39b21b23f2d8d1eb83b5ab0b595c2ae66d956
jihogrammer <jihogrammer@gmail.com>
2023-12-19 18:50:59 +0900

    create validation application

---
+++ validation/src/main/java/dev/jihogrammer/item/ItemValidationApplication.java
@@ -0,0 +1,11 @@
+package dev.jihogrammer.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemValidationApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(ItemValidationApplication.class, args);
+    }
+}
