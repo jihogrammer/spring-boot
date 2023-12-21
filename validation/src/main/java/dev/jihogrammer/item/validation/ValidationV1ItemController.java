a69c27f2b527cf4382c610bcb403facf3630e3d9
jihogrammer <jihogrammer@gmail.com>
2023-12-21 16:37:36 +0900

    feat validation for fields and global rule using a Map

--- validation/src/main/java/dev/jihogrammer/item/validation/ValidationV1ItemController.java
+++ validation/src/main/java/dev/jihogrammer/item/validation/ValidationV1ItemController.java
@@ -4,7 +4,6 @@ import dev.jihogrammer.item.ItemService;
 import dev.jihogrammer.item.model.in.ItemRegisterHttpRequest;
 import dev.jihogrammer.item.model.in.ItemUpdateHttpRequest;
 import dev.jihogrammer.item.model.out.ItemView;
-import dev.jihogrammer.items.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +12,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/validation/v1/items")
@@ -40,9 +42,20 @@ public class ValidationV1ItemController {
 
     @PostMapping("/register")
     public String registerItem(final Model model, final ItemRegisterHttpRequest request, final RedirectAttributes redirectAttributes) {
-        ItemView itemView = this.service.register(request.mapToCommand());
-        redirectAttributes.addAttribute("itemId", itemView.id());
-        return "redirect:/validation/v1/items/{itemId}";
+        Map<String, String> errorMap = validateRequest(request);
+
+        // validated case
+        if (errorMap.isEmpty()) {
+            ItemView itemView = this.service.register(request.mapToCommand());
+            redirectAttributes.addAttribute("itemId", itemView.id());
+            return "redirect:/validation/v1/items/{itemId}";
+        }
+        // not validated case
+        else {
+            model.addAttribute("item", request);
+            model.addAttribute("errors", errorMap);
+            return "/validation/v1-item-register";
+        }
     }
 
     @GetMapping("/update/{itemId}")
@@ -52,9 +65,65 @@ public class ValidationV1ItemController {
     }
 
     @PostMapping("/update/{itemId}")
-    public String updateItem(final ItemUpdateHttpRequest request, final RedirectAttributes redirectAttributes) {
-        ItemView itemView = this.service.update(request.mapToCommand());
-        redirectAttributes.addAttribute("itemId", itemView.id());
-        return "redirect:/validation/v1/items/{itemId}";
+    public String updateItem(final ItemUpdateHttpRequest request, final RedirectAttributes redirectAttributes, final Model model) {
+        Map<String, String> errorMap = validateRequest(request);
+
+        // validated case
+        if (errorMap.isEmpty()) {
+            ItemView itemView = this.service.update(request.mapToCommand());
+            redirectAttributes.addAttribute("itemId", itemView.id());
+            return "redirect:/validation/v1/items/{itemId}";
+        }
+        // not validated case
+        else {
+            model.addAttribute("item", request);
+            model.addAttribute("errors", errorMap);
+            return "/validation/v1-item-update";
+        }
+    }
+
+
+    private Map<String, String> validateRequest(final ItemRegisterHttpRequest request) {
+        final Map<String, String> errorMap = new HashMap<>();
+
+        // single field validation
+        if (request.getName() == null || request.getName().isEmpty()) {
+            errorMap.put("name", "이름은 꼭 입력해주세요.");
+        }
+        if (request.getPrice() == null || (1_000 > request.getPrice() || request.getPrice() > 1_000_000)) {
+            errorMap.put("price", "가격은 1,000 ~ 1,000,000 원 사이의 값으로 정해주세요.");
+        }
+        if (request.getQuantity() == null || (1 > request.getQuantity() || request.getQuantity() > 10_000)) {
+            errorMap.put("quantity", "수량은 0 ~ 9,999 개까지 입력해주세요.");
+        }
+
+        // complex fields validation
+        if (request.getPrice() != null && request.getQuantity() != null && (request.getPrice() * request.getQuantity() < 10_000)) {
+            errorMap.put("global", "(가격 * 수량 >= 10_000) 조건이 만족시켜주세요(현재: " + (request.getPrice() * request.getQuantity()) + ").");
+        }
+
+        return errorMap;
+    }
+
+    private Map<String, String> validateRequest(final ItemUpdateHttpRequest request) {
+        final Map<String, String> errorMap = new HashMap<>();
+
+        // single field validation
+        if (request.getName() == null || request.getName().isEmpty()) {
+            errorMap.put("name", "이름은 꼭 입력해주세요.");
+        }
+        if (request.getPrice() == null || (1_000 > request.getPrice() || request.getPrice() > 1_000_000)) {
+            errorMap.put("price", "가격은 1,000 ~ 1,000,000 원 사이의 값으로 정해주세요.");
+        }
+        if (request.getQuantity() == null || (1 > request.getQuantity() || request.getQuantity() > 10_000)) {
+            errorMap.put("quantity", "수량은 0 ~ 9,999 개까지 입력해주세요.");
+        }
+
+        // complex fields validation
+        if (request.getPrice() != null && request.getQuantity() != null && (request.getPrice() * request.getQuantity() < 10_000)) {
+            errorMap.put("global", "(가격 * 수량 >= 10_000) 조건이 만족시켜주세요(현재: " + (request.getPrice() * request.getQuantity()) + ").");
+        }
+
+        return errorMap;
     }
 }
