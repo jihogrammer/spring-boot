a9f1d06bdc5f1494cecd8d0d9adb26c9cc6783b1
jihogrammer <jihogrammer@gmail.com>
2024-01-05 20:57:14 +0900

    create login applcation module

---
+++ items-login-application/src/main/java/dev/jihogrammer/item/login/LoginApplication.java
@@ -0,0 +1,11 @@
+package dev.jihogrammer.item.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(LoginApplication.class, args);
+    }
+}
---
+++ items-login-application/src/main/java/dev/jihogrammer/item/login/home/HomeController.java
@@ -0,0 +1,12 @@
+package dev.jihogrammer.item.login.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String homePage() {
+        return "home";
+    }
+}
