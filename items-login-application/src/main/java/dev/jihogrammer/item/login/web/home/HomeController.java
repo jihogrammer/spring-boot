e2ea6149f9c0fb99cb2aa1fba28cb63b26abbb5d
jihogrammer <jihogrammer@gmail.com>
2024-01-13 15:55:09 +0900

    convert HomeController to use HttpServletRequest Session

--- items-login-application/src/main/java/dev/jihogrammer/item/login/web/home/HomeController.java
+++ items-login-application/src/main/java/dev/jihogrammer/item/login/web/home/HomeController.java
@@ -2,30 +2,21 @@ package dev.jihogrammer.item.login.web.home;
 
 import dev.jihogrammer.item.login.web.SessionConstant;
 import dev.jihogrammer.member.Member;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
-import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String homePage(
-        final HttpServletRequest httpServletRequest,
+        @SessionAttribute(name = SessionConstant.LOGGED_IN_MEMBER, required = false) final Member member,
         final Model model
     ) {
-        HttpSession httpSession = httpServletRequest.getSession(false);
-
-        if (isNull(httpSession)) {
-            return "home";
-        }
-
-        Object member = httpSession.getAttribute(SessionConstant.LOGGED_IN_MEMBER);
-        if (nonNull(member) && Member.class.isAssignableFrom(member.getClass())) {
+        if (nonNull(member)) {
             model.addAttribute("member", member);
             return "member-home";
         }
