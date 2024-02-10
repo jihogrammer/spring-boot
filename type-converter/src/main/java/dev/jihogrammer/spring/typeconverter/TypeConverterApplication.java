ec1f8b2db45256ca4fcdc249cbf5a7e74652e889
jihogrammer <jihogrammer@gmail.com>
2024-02-10 15:31:17 +0900

    create type-converter module

---
+++ type-converter/src/main/java/dev/jihogrammer/spring/typeconverter/TypeConverterApplication.java
@@ -0,0 +1,11 @@
+package dev.jihogrammer.spring.typeconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeConverterApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(TypeConverterApplication.class, args);
+    }
+}
