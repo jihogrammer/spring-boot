8ca6100eca26aa2718fbef8cae27b71630083830
jihogrammer <jihogrammer@gmail.com>
2024-02-10 15:42:12 +0900

    add hello controller
    - HttpServletRequest에서 받은 데이터는 모두 String 타입
    - Spring 내부 Converter로 변환되어 @RequestParam 등으로 사용 가능

--- type-converter/src/main/java/dev/jihogrammer/spring/typeconverter/TypeConverterApplication.java
+++ type-converter/src/main/java/dev/jihogrammer/spring/typeconverter/TypeConverterApplication.java
@@ -1,11 +1,34 @@
 package dev.jihogrammer.spring.typeconverter;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class TypeConverterApplication {
     public static void main(final String[] args) {
         SpringApplication.run(TypeConverterApplication.class, args);
+    }
+
+    @RestController
+    @Slf4j
+    public static class HelloController {
+        @GetMapping("/hello-v1")
+        public String helloV1(final HttpServletRequest request) {
+            String rawData = request.getParameter("data");
+            Integer integerData = Integer.valueOf(rawData);
+            log.info("data = {}", integerData);
+            return "ok";
+        }
+
+        @GetMapping("/hello-v2")
+        public String helloV2(@RequestParam final Integer data) {
+            log.info("data = {}", data);
+            return "ok";
+        }
     }
 }
