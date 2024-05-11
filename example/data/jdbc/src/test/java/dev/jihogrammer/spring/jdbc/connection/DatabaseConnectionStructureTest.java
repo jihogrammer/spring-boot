2233224e604d3189f51783da8da7a9414164b575
jihogrammer <jihogrammer@gmail.com>
2024-05-11 17:52:45 +0900

    Test java.sql.DriverManager and DriverManagerDataSource

---
+++ example/data/jdbc/src/test/java/dev/jihogrammer/spring/jdbc/connection/DatabaseConnectionStructureTest.java
@@ -0,0 +1,72 @@
+package dev.jihogrammer.spring.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest(classes = ConnectionDataTestConfig.class)
+class DatabaseConnectionStructureTest {
+
+    @Autowired
+    ConnectionDataTestConfig config;
+
+    /**
+     * Java 내부 JDBC 사용을 위한 방법
+     * <br>
+     * 항상 새로운 Connection 객체를 생성한다.
+     *
+     * @see DriverManager
+     */
+    @Test
+    void testDriverManagerConnection() throws Throwable {
+        // given
+        final Connection connection1;
+        final Connection connection2;
+
+        // when
+        connection1 = DriverManager.getConnection(config.url(), config.username(), config.password());
+        connection2 = DriverManager.getConnection(config.url(), config.username(), config.password());
+        log.info("java.sql.DriverManager#getConnection -> {}", connection1);
+        log.info("java.sql.DriverManager#getConnection -> {}", connection2);
+
+        // then
+        assertThat(connection1).isNotNull();
+        assertThat(connection2).isNotNull();
+        assertThat(connection1).isNotEqualTo(connection2);
+    }
+
+    /**
+     * Spring 내부 JDBC 사용을 위한 방법
+     * <br>
+     * java.sql.DriverManager 방식과 마찬가지로 항상 새로운 Connection 객체를 생성한다.
+     *
+     * @see DriverManagerDataSource
+     */
+    @Test
+    void testDriverManagerDataSourceConnection() throws Throwable {
+        // given
+        final var dataSource = new DriverManagerDataSource(config.url(), config.username(), config.password());
+        final Connection connection1;
+        final Connection connection2;
+
+        // when
+        connection1 = dataSource.getConnection();
+        connection2 = dataSource.getConnection();
+        log.info("DriverManagerDataSource#getConnection -> {}", connection1);
+        log.info("DriverManagerDataSource#getConnection -> {}", connection2);
+
+        // then
+        assertThat(connection1).isNotNull();
+        assertThat(connection2).isNotNull();
+        assertThat(connection1).isNotEqualTo(connection2);
+    }
+
+}
