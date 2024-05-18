484923e4e878b99cde81f16b535e5ab74ad27c8c
jihogrammer <jihogrammer@gmail.com>
2024-05-18 14:30:50 +0900

    Config DataSource from application properties

--- example/data/jdbc/src/main/java/dev/jihogrammer/spring/jdbc/member/adaptor/persistence/out/MemberPersistenceAdaptorFactory.java
+++ example/data/jdbc/src/main/java/dev/jihogrammer/spring/jdbc/member/adaptor/persistence/out/MemberPersistenceAdaptorFactory.java
@@ -14,7 +14,8 @@ import javax.sql.DataSource;
 @Configuration
 public class MemberPersistenceAdaptorFactory {
 
-    @Bean
+    // @Bean
+    // spring.datasource.xxx 형식으로 설정하면 자동으로 DataSource 빈을 스프링이 알아서 등록한다.
     public DataSource dataSource(
         @Value("${spring.datasource.url}") final String url,
         @Value("${spring.datasource.username}") final String username,
