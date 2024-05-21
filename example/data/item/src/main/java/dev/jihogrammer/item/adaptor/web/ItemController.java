53d8adbc40fa1ac1401ba5a58ec1aabd28384dcb
jihogrammer <jihogrammer@gmail.com>
2024-05-21 22:47:20 +0900

    Feat item views

---
+++ example/data/item/src/main/java/dev/jihogrammer/item/adaptor/web/ItemController.java
@@ -0,0 +1,112 @@
+package dev.jihogrammer.item.adaptor.web;
+
+import dev.jihogrammer.item.adaptor.web.entity.RegisterPayload;
+import dev.jihogrammer.item.adaptor.web.entity.SearchPayload;
+import dev.jihogrammer.item.adaptor.web.entity.UpdatePayload;
+import dev.jihogrammer.item.application.port.in.ItemQuery;
+import dev.jihogrammer.item.application.port.in.ItemUpdatePort;
+import dev.jihogrammer.item.application.port.out.ItemSaveCommand;
+import dev.jihogrammer.item.domain.Item;
+import dev.jihogrammer.item.domain.ItemId;
+import dev.jihogrammer.item.domain.exception.ItemException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/items")
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemQuery itemQuery;
+
+    private final ItemUpdatePort itemUpdatePort;
+
+    @GetMapping
+    public String items(
+            final Model model,
+            @ModelAttribute("payload") final SearchPayload payload
+    ) {
+        final var command = payload == null ? null : payload.toCommand();
+        final var items = this.itemQuery.search(command);
+
+        model.addAttribute("items", items);
+
+        return "items";
+    }
+
+    @GetMapping("/{id}")
+    public String item(@PathVariable("id") final String id, final Model model) {
+        final var optionalItem = this.itemQuery.findById(new ItemId(id));
+
+        if (optionalItem.isEmpty()) {
+            throw new ItemException("Could not find a member.");
+        }
+
+        model.addAttribute("item", optionalItem.get());
+
+        return "item";
+    }
+
+    @GetMapping("/register")
+    public String register(final Model model) {
+        model.addAttribute("payload", new RegisterPayload());
+
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(
+            @ModelAttribute("payload") final RegisterPayload payload,
+            final RedirectAttributes redirectAttributes
+    ) {
+        final var command = payload.toCommand();
+        final var item = this.itemUpdatePort.register(command);
+
+        redirectAttributes.addAttribute("id", item.id().value());
+
+        return "redirect:/items/{id}";
+    }
+
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable("id") final String id, final Model model) {
+        final var optionalItem = this.itemQuery.findById(new ItemId(id));
+
+        if (optionalItem.isEmpty()) {
+            throw new ItemException("Could not find a item.");
+        }
+
+        final var payload = UpdatePayload.of(optionalItem.get());
+
+        model.addAttribute("payload", payload);
+
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String update(
+            @ModelAttribute("payload") final UpdatePayload payload,
+            final RedirectAttributes redirectAttributes
+    ) {
+        final var command = payload.toCommand();
+        final var item = this.itemUpdatePort.register(command);
+
+        redirectAttributes.addAttribute("id", item.id().value());
+
+        return "redirect:/items/{id}";
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    void createMockItems() {
+        this.itemUpdatePort.register(new ItemSaveCommand(null, "test-item-1", 1000, 10));
+        this.itemUpdatePort.register(new ItemSaveCommand(null, "test-item-2", 2000, 20));
+        this.itemUpdatePort.register(new ItemSaveCommand(null, "test-item-3", 3000, 30));
+    }
+
+}
---
+++ example/data/item/src/main/java/dev/jihogrammer/item/adaptor/web/entity/UpdatePayload.java
@@ -0,0 +1,33 @@
+package dev.jihogrammer.item.adaptor.web.entity;
+
+import dev.jihogrammer.item.application.port.out.ItemSaveCommand;
+import dev.jihogrammer.item.domain.Item;
+import lombok.Data;
+
+@Data
+public class UpdatePayload {
+
+    private String id;
+
+    private String name;
+
+    private Integer price;
+
+    private Integer quantity;
+
+    public static UpdatePayload of(final Item item) {
+        UpdatePayload payload = new UpdatePayload();
+
+        payload.id = item.id().value();
+        payload.name = item.name();
+        payload.price = item.price();
+        payload.quantity = item.quantity();
+
+        return payload;
+    }
+
+    public ItemSaveCommand toCommand() {
+        return new ItemSaveCommand(this.id, this.name, this.price, this.quantity);
+    }
+
+}
