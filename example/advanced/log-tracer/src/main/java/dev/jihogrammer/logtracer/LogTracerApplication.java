096b65ce16f1c963d00005f39f12d4ff4cd9afef
jihogrammer <jihogrammer@gmail.com>
2024-06-08 12:22:18 +0900

    Feat Field Log Tracer

--- example/advanced/log-tracer/src/main/java/dev/jihogrammer/logtracer/LogTracerApplication.java
+++ example/advanced/log-tracer/src/main/java/dev/jihogrammer/logtracer/LogTracerApplication.java
@@ -3,7 +3,7 @@ package dev.jihogrammer.logtracer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "dev.jihogrammer.logtracer.adaptor.v2")
+@SpringBootApplication(scanBasePackages = "dev.jihogrammer.logtracer.adaptor.v3")
 public class LogTracerApplication {
 
     public static void main(final String[] args) {
