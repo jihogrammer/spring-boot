cf8864f3d0e3e670289bccbbda54676c8da01211
jihogrammer <jihogrammer@gmail.com>
2024-10-04 15:31:06 +0900

    Refactor V1AspectTest

--- example/advanced/log-tracer/src/test/java/dev/jihogrammer/aop/V1AspectTest.java
+++ example/advanced/log-tracer/src/test/java/dev/jihogrammer/aop/V1AspectTest.java
@@ -1,26 +1,28 @@
-package dev.jihogrammer.aop.order;
+package dev.jihogrammer.aop;
 
-import dev.jihogrammer.aop.AopApplication;
 import dev.jihogrammer.aop.aspect.V1Aspect;
+import dev.jihogrammer.aop.order.OrderRepository;
+import dev.jihogrammer.aop.order.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.springframework.aop.support.AopUtils.isAopProxy;
 
 @Slf4j
 @SpringBootTest(classes = {AopApplication.class, V1Aspect.class})
-class AopTest {
+class V1AspectTest {
 
     @Test
     void logInfo(
             @Autowired final OrderRepository repository,
             @Autowired final OrderService service
     ) {
-        log.info("isAopProxy(repository) -> {}", isAopProxy(repository));
-        log.info("isAopProxy(service) -> {}", isAopProxy(service));
+        assertThat(isAopProxy(repository)).isTrue();
+        assertThat(isAopProxy(service)).isTrue();
     }
 
     @Test
