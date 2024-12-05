aed9ce3d7b64b1eb571e2b2b8fc6cddf1be02474
jihogrammer <jihogrammer@gmail.com>
2024-12-05 15:53:27 +0900

    Create bean using @ConfigurationProperties

--- example/advanced/external-config/src/main/java/dev/jihogrammer/spring/boot/external/ExternalConfigApplication.java
+++ example/advanced/external-config/src/main/java/dev/jihogrammer/spring/boot/external/ExternalConfigApplication.java
@@ -1,6 +1,7 @@
 package dev.jihogrammer.spring.boot.external;
 
 import dev.jihogrammer.spring.boot.external.config.DataSourceEnvConfig;
+import dev.jihogrammer.spring.boot.external.config.DataSourceV1PropertiesConfig;
 import dev.jihogrammer.spring.boot.external.config.DataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,8 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = "dev.jihogrammer.spring.boot.external.datasource")
 //@Import(DataSourceEnvConfig.class)
-@Import(DataSourceValueConfig.class)
+//@Import(DataSourceValueConfig.class)
+@Import(DataSourceV1PropertiesConfig.class)
 public class ExternalConfigApplication {
 
     public static void main(final String[] args) {
---
+++ example/advanced/external-config/src/main/java/dev/jihogrammer/spring/boot/external/config/DataSourceV1PropertiesConfig.java
@@ -0,0 +1,22 @@
+package dev.jihogrammer.spring.boot.external.config;
+
+import dev.jihogrammer.spring.boot.external.datasource.CustomDataSource;
+import dev.jihogrammer.spring.boot.external.datasource.V1CustomDataSourceProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@EnableConfigurationProperties(V1CustomDataSourceProperties.class)
+public class DataSourceV1PropertiesConfig {
+
+    @Bean
+    CustomDataSource dataSource(final V1CustomDataSourceProperties properties) {
+        return new CustomDataSource(
+                properties.getUrl(),
+                properties.getUsername(),
+                properties.getPassword(),
+                properties.getConfig().getMaxConnection(),
+                properties.getConfig().getTimeout(),
+                properties.getConfig().getOptions());
+    }
+
+}
