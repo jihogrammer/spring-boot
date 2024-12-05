a960da100887666f2192f47eeceee69bbf885273
jihogrammer <jihogrammer@gmail.com>
2024-12-05 12:30:19 +0900

    Create external-config module

---
+++ example/advanced/external-config/src/main/java/dev/jihogrammer/spring/boot/external/ExternalConfigApplication.java
@@ -0,0 +1,13 @@
+package dev.jihogrammer.spring.boot.external;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalConfigApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ExternalConfigApplication.class, args);
+    }
+
+}
