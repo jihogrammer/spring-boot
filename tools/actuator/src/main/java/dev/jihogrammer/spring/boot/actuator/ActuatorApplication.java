7db59f9650ad87a120a4285e764008dc4defaa14
jihogrammer <jihogrammer@gmail.com>
2024-12-06 15:21:27 +0900

    Create actuator module

---
+++ tools/actuator/src/main/java/dev/jihogrammer/spring/boot/actuator/ActuatorApplication.java
@@ -0,0 +1,13 @@
+package dev.jihogrammer.spring.boot.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+}
