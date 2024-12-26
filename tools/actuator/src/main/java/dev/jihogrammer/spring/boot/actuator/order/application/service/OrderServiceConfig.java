6b82c3012b37f9a2854d4aa639bae58688a6b549
jihogrammer <jihogrammer@gmail.com>
2024-12-26 14:28:17 +0900

    Apply @Timed on V4OrderService class

--- tools/actuator/src/main/java/dev/jihogrammer/spring/boot/actuator/order/application/service/OrderServiceConfig.java
+++ tools/actuator/src/main/java/dev/jihogrammer/spring/boot/actuator/order/application/service/OrderServiceConfig.java
@@ -1,6 +1,7 @@
 package dev.jihogrammer.spring.boot.actuator.order.application.service;
 
 import dev.jihogrammer.spring.boot.actuator.order.application.port.in.OrderCoordinator;
+import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +10,13 @@ import org.springframework.context.annotation.Configuration;
 class OrderServiceConfig {
 
     @Bean
-    OrderCoordinator orderCoordinator(final MeterRegistry meterRegistry) {
-        return new V3OrderService(meterRegistry);
+    OrderCoordinator orderCoordinator() {
+        return new V4OrderService();
+    }
+
+    @Bean
+    TimedAspect timedAspect(final MeterRegistry registry) {
+        return new TimedAspect(registry);
     }
 
 }
