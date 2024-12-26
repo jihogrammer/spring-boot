71ebde797c887ed4b8cfe155506db377c0655a20
jihogrammer <jihogrammer@gmail.com>
2024-12-26 14:40:49 +0900

    Register stock gauge

--- tools/actuator/src/main/java/dev/jihogrammer/spring/boot/actuator/order/application/service/OrderServiceConfig.java
+++ tools/actuator/src/main/java/dev/jihogrammer/spring/boot/actuator/order/application/service/OrderServiceConfig.java
@@ -2,7 +2,11 @@ package dev.jihogrammer.spring.boot.actuator.order.application.service;
 
 import dev.jihogrammer.spring.boot.actuator.order.application.port.in.OrderCoordinator;
 import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +21,29 @@ class OrderServiceConfig {
     @Bean
     TimedAspect timedAspect(final MeterRegistry registry) {
         return new TimedAspect(registry);
+    }
+
+    @Bean
+    GaugeMetric gaugeMetric(final OrderCoordinator orderCoordinator, final MeterRegistry registry) {
+        return new GaugeMetric(orderCoordinator, registry);
+    }
+
+    @Slf4j
+    @RequiredArgsConstructor
+    static class GaugeMetric {
+
+        private final OrderCoordinator orderCoordinator;
+
+        private final MeterRegistry registry;
+
+        @PostConstruct
+        void init() {
+            Gauge.builder("my.stock", this.orderCoordinator, (service) -> {
+                log.info("[STOCK]");
+                return service.getStock().get();
+            }).register(this.registry);
+        }
+
     }
 
 }
