2012717d42c497316c4ba70bbf21b10c94b7e8d3
jihogrammer <jihogrammer@gmail.com>
2024-12-26 14:18:00 +0900

    Feat timer metric

--- tools/actuator/src/main/java/dev/jihogrammer/spring/boot/actuator/order/application/service/OrderServiceConfig.java
+++ tools/actuator/src/main/java/dev/jihogrammer/spring/boot/actuator/order/application/service/OrderServiceConfig.java
@@ -1,7 +1,6 @@
 package dev.jihogrammer.spring.boot.actuator.order.application.service;
 
 import dev.jihogrammer.spring.boot.actuator.order.application.port.in.OrderCoordinator;
-import io.micrometer.core.aop.CountedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,13 +9,8 @@ import org.springframework.context.annotation.Configuration;
 class OrderServiceConfig {
 
     @Bean
-    OrderCoordinator orderCoordinator() {
-        return new V2OrderService();
-    }
-
-    @Bean
-    CountedAspect countedAspect(final MeterRegistry registry) {
-        return new CountedAspect(registry);
+    OrderCoordinator orderCoordinator(final MeterRegistry meterRegistry) {
+        return new V3OrderService(meterRegistry);
     }
 
 }
